9942212f08482611a4b1033aece6f08a3d976a92
JGWill <jgi@jgwill.com>
2024-03-27 22:51:10 -0400

    fix

--- gia-ast-server.js
+++ gia-ast-server.js
@@ -1,61 +1,69 @@
+#!/usr/bin/env node
 
 //Run server hosted on HuggingFace
 
 //docker rm astpicasso --force
 const valid_choices_are = "picasso,van-gogh,roerich,pollock,peploe,munch,monet,kirchner,gauguin,el-greco,cezanne,morisot";
 
-const { exec } = require('child_process');
-//if cli called with "--picasso", run command above
-if (process.argv[2]) {
+function main() {
 
-  //if the argument is not in the list of valid choices, exit
-  if (!valid_choices_are.includes(process.argv[2])) {
-    show_valid_choices();
-    return;
-  }
-  tmp_arg = process.argv[2].replace("--", "").replace("ast", "");
-  container_name = "ast" + tmp_arg;
+  const { exec } = require('child_process');
+  //if cli called with "--picasso", run command above
+  if (process.argv[2]) {
 
-  if (process.argv[3] == "--rm") {
-    //remove the container
-    exec("docker rm " + container_name + " --force", (err, stdout, stderr) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      console.log("container " + container_name + " stopped and removed");
-      //exit the process
+    model_name = process.argv[2].replace("--", "").replace("ast", "");
+    container_name = "ast" + model_name;
 
-
-    });
-  }
-  else {
-
-
-
-    var local_port = 7860;
-    if (process.argv[3]) {
-      //local port to run the server on
-      local_port = process.argv[3];
+    //if the argument is not in the list of valid choices, exit
+    if (!valid_choices_are.includes(model_name)) {
+      show_valid_choices();
+      process.exit(1);
     }
 
-    var cmd_launcher = create_docker_run(ast_model = container_name, local_port = local_port)
+    if (process.argv[3] == "--rm") {
+      //remove the container
+      exec("docker rm " + container_name + " --force", (err, stdout, stderr) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log("container " + container_name + " stopped and removed");
+        //exit the process
 
 
-    exec(cmd_launcher, (err, stdout, stderr) => {
-      if (err) {
-        console.error(err);
-        return;
+      });
+    }
+    else {
+
+
+
+      var local_port = 7860;
+      if (process.argv[3]) {
+        //local port to run the server on
+        local_port = process.argv[3];
       }
-      console.log(stdout + "\n docker container named" + container_name + " running on port " + local_port);
-      console.log("docker stop " + container_name + " --force #to remove");
-      console.log("  the likely url for the API is http://localhost:" + local_port + "/stylize");
 
-    });
+      var cmd_launcher = create_docker_run(ast_model = container_name, local_port = local_port)
+
+
+      exec(cmd_launcher, (err, stdout, stderr) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(stdout + "\n docker container named" + container_name + " running on port " + local_port);
+        //console.log("docker stop " + container_name + " --force #to remove");
+        console.log("  to stop the container, run : gia-ast-server --" + container_name.replace("ast","") + " --rm");
+        console.log("  the likely url for the API is http://localhost:" + local_port + "/stylize");
+
+      });
+    }
+
   }
-}
-else {
-  show_valid_choices();
+  else {
+    show_valid_choices();
+    process.exit(1);
+  }
 }
 
 function show_valid_choices() {
@@ -70,3 +78,7 @@ function create_docker_run(ast_model = "astpicasso", local_port = 7860) {
   console.log(result);
   return result;
 }
+
+
+
+main();
